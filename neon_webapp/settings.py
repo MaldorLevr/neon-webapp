6d2e81647556efc6595e7cdd90fe6b22eea3de94
Vadym Martsynovskyy <maldorlevr@gmail.com>
2016-02-02 08:46:34 -0800

    testign stuff

--- neon_webapp/settings.py
+++ neon_webapp/settings.py
@@ -104,12 +104,7 @@ if os.environ.get('SECRET_KEY'):
     STATIC_ROOT = '/home/django/static/'
 
     ALLOWED_HOSTS = [
-        '127.0.0.1',
-        u'127.0.0.1',
-        '107.170.252.240',
-        'windsorapp.me',
-        '.windsorapp.me',
-        'windsorapp.me.',
+        '*',
     ]
 else:
     SECRET_KEY = 'v$n#racg8iqp4d*s+3k@cc^svw7qln@z6%ercj+iw$ub+@a#ma'
