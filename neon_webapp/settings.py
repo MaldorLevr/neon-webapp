590775ec26327c57f4e526e5a8141c2363356647
Vadym Martsynovskyy <maldorlevr@gmail.com>
2016-04-06 12:44:59 -0700

    addded logging back

--- neon_webapp/settings.py
+++ neon_webapp/settings.py
@@ -117,6 +117,25 @@ if os.environ.get('SECRET_KEY'):
                      '107.170.252.240',
                      'windsorapp.me', ]
 
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'file': {
+                'level': 'DEBUG',
+                'class': 'logging.FileHandler',
+                'filename': '/root/django/debug.log',
+            },
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['file'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+        },
+    }
+
 else:
     SECRET_KEY = 'v$n#racg8iqp4d*s+3k@cc^svw7qln@z6%ercj+iw$ub+@a#ma'
     DEBUG = True
