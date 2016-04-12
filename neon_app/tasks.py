e930cfdcd24e0fd14550406f11125fa9a69033c4
Vadym Martsynovskyy <maldorlevr@gmail.com>
2016-04-12 09:10:17 -0700

    fixed some stuff in the calendar task

--- neon_app/tasks.py
+++ neon_app/tasks.py
@@ -1,20 +1,23 @@
-from huey.contrib.djhuey import crontab, db_periodic_task, db_task
+from huey.contrib.djhuey import crontab, db_periodic_task
 from neon_app.models import Day
 from icalendar import Calendar
-from datetime import datetime
 import requests
 
 pro_d_terms = ['Professional', 'Pro-D', 'Development']
 holiday_terms = ['Holiday', 'Statutory', 'School Closed']
 late_start_terms = ['Late Start']
 
+
 @db_periodic_task(crontab(minute="*/5"))
 def update_from_calendar():
     print("hello")
-    r = requests.get('http://www.sd44.ca/school/windsor/_LAYOUTS/15/scholantis/handlers/ical/event.ashx?List=f13b021f-ee41-4705-ab17-1a2f36172f0b')
+    r = requests.get("http://www.sd44.ca/school/windsor/"
+                     "_LAYOUTS/15/scholantis/handlers/ical/"
+                     "event.ashx?List=f13b021f-ee41-4705-ab17-1a2f36172f0b")
     cal = Calendar.from_ical(r.text)
     for event in (x for x in cal.subcomponents if x.name == 'VEVENT'):
-        days = Day.objects.filter(name=event['summary'])
+        days = Day.objects.filter(name=event['summary']) \
+            .filter(date=event['dtstart'].dt)
         if len(days) > 0:
             continue
         day_type = ""
@@ -25,5 +28,19 @@ def update_from_calendar():
         elif any(x in event['summary'] for x in pro_d_terms):
             day_type = "pro-d"
 
-        day = Day(date=event['dtstart'].dt, name=event['summary'], day_type=day_type, announcement=event['description'])
+        if 'name' in event:
+            name = event['summary']
+        else:
+            name = "Normal Day"
+
+        if 'description' in event:
+            announcement = event['description']
+        else:
+            announcement = ""
+
+        day = Day(date=event['dtstart'].dt,
+                  name=name,
+                  day_type=day_type,
+                  announcement=announcement)
+
         day.save()
